026cdc594a7034e2e5e4e93c21367ff24a93cb32
AAS696969 <138122939+AAS696969@users.noreply.github.com>
2024-12-05 14:33:57 +0300

    Add files via upload

---
+++ module_3_hard.py
@@ -0,0 +1,17 @@
+def calculate_structure_sum(data_structure):
+    sum=0
+    if isinstance(data_structure, int):
+        sum += data_structure
+    elif isinstance(data_structure, str):
+        sum += len(data_structure)
+    elif isinstance(data_structure, (list, tuple, set)):
+        for x in data_structure:
+            sum += calculate_structure_sum(x)
+    elif isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            sum += calculate_structure_sum(key)
+            sum += calculate_structure_sum(value)
+    return sum
+data_structure = [ [1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}]) ]
+result = calculate_structure_sum(data_structure)
+print(result)
